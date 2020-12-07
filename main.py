3b835d993ef82a3b988a7888e7954749187036b0
Mikael Morrell-Stinson <67693304+Mikael-MStinson@users.noreply.github.com>
2020-12-07 15:20:17 -0600

    Tinkering with possible behavior
    I'm still not entirely sure what the behavior of this tool should be. I'll need to look into it more.

---
+++ main.py
@@ -0,0 +1,25 @@
+class PTO:
+	def __init__(self, pto_type, date_earned, date_expire, amount = 1.0, claims = []):
+		self.pto_type = pto_type
+		self.date_earned = date_earned
+		self.date_earned = date_expire
+		self.amount = amount
+		self.claims = claims
+		
+	
+
+def claim_time(record, current_date, amount_claimed, reason):
+	pass
+	
+def earn_time(record, current_date, amount_earned, pto_type):
+	pass
+	
+def record_to_json(record):
+	pass
+	
+def json_to_record(json):
+	pass
+	
+def report_time(self):
+	pass
+	
